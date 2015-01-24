095149873240ce8d3dfbffb35fe17ab05c88dfb4
Barbie <barbie@missbarbell.co.uk>
2015-01-24 17:38:55 +0000

    updated copyright year

--- lib/WWW/Scraper/ISBN.pm
+++ lib/WWW/Scraper/ISBN.pm
@@ -240,12 +240,12 @@ the given isbn.
 =head1 AUTHOR
 
   2004-2013 Andy Schamp, E<lt>andy@schamp.netE<gt>
-  2013-2014 Barbie, E<lt>barbie@cpan.orgE<gt>
+  2013-2015 Barbie, E<lt>barbie@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
   Copyright 2004-2013 by Andy Schamp
-  Copyright 2013-2014 by Barbie
+  Copyright 2013-2015 by Barbie
 
   This distribution is free software; you can redistribute it and/or
   modify it under the Artistic Licence v2.
