32c3541709b33609c5b1cac9296ea0faa281e5f0
Barbie <barbie@missbarbell.co.uk>
2019-08-26 15:50:22 +0000

    updated version

--- lib/WWW/Scraper/ISBN/Driver.pm
+++ lib/WWW/Scraper/ISBN/Driver.pm
@@ -3,7 +3,7 @@ package WWW::Scraper::ISBN::Driver;
 use strict;
 use warnings;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 #----------------------------------------------------------------------------
 # Library Modules
--- lib/WWW/Scraper/ISBN/Record.pm
+++ lib/WWW/Scraper/ISBN/Record.pm
@@ -3,7 +3,7 @@ package WWW::Scraper::ISBN::Record;
 use strict;
 use warnings;
 
-our $VERSION = '1.04';
+our $VERSION = '1.05';
 
 #----------------------------------------------------------------------------
 # Public API
